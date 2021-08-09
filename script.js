d99037653fd98515ddded10e9027feb298cd5943
NatanWS <natanwsn@gmail.com>
2021-08-09 14:29:35 -0300

    corrigindo falha na criação de id/classe

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ function createBoard() {
 
     for (let i = 0; i < 7; i++) {
         let boardColumn = document.createElement('div')
-        boardColumn.setAttribute('id', `column__${i+1}`)
+        boardColumn.setAttribute('id', `column__${i}`)
         boardColumn.classList.add('column')
         gameContainer.appendChild(boardColumn)
 
         for (let j = 0; j < 6; j++) {
             let boardLine = document.createElement('div')
-            boardLine.setAttribute('id', `line__${j+1}`)
+            boardLine.classList.add(`line__${j}`)
             boardLine.classList.add('line')
             boardColumn.appendChild(boardLine)
         }
