47e952f1905f8d73effacc1cfa8d95a591a29177
NatanWS <natanwsn@gmail.com>
2021-08-09 14:32:41 -0300

    ajustada criação de disco

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let tabuleiro = [
 
 function createDisk(column, line) {
     let currentColumn = document.getElementById(`column__${column}`)
-    let currentLine = currentColumn.getElementById(`line__${line}`)
+    let currentLine = currentColumn.getElementsByClassName(`line__${line}`)
     let newDisk = document.createElement('div')
     newDisk.classList.add('disk')
     currentLine.appendChild(newDisk)
