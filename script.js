1d8c77e7fd00dc78998180433f690931ea5334a8
Matheus Bueno <matheus_bueno10@hotmail.com>
2021-08-09 15:46:31 -0300

    Adicionado function bottomToTop

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let tabuleiro = [
 [0,0,0,0,0,0],
 ];
 
+
 function checkDiagonalTopToBottom(){
 	for(let line = 0; line < 4; line++){
 		for(let column = 0; column < 4; column++){
@@ -51,15 +52,39 @@ function checkDiagonalTopToBottom(){
 			}
 		}
 	}
-	for(let line = 0; line < 4; line++){
-		for(let column = 0; column < 4; column++){
-			
-		}
-	}
 	return console.log("continue jogando")
 }
 
 function checkDiagonalBottomToTop(){
-	
+	for(let line = 6; line >= 3; line--){
+		for(let column = 0; column < 3 ; column++){
+			let one = tabuleiro[line][column];
+			let two = tabuleiro[line - 1][column + 1];
+			let three = tabuleiro[line - 2][column + 2];
+			let four = tabuleiro[line - 3][column + 3];
+			if(one === 1 && one !== 0 && one === two && one === three && one === four){
+				return console.log("Player1 Win")
+			}
+			if(one === 2 && one !== 0 && one === two && one === three && one === four){
+				return console.log("Player2 Win")
+			}
+		}
+	}
+	for(let line = 5; line >= 3; line--){
+		for(let column = 0; column < 4 ; column++){
+			let one = tabuleiro[line][column];
+			let two = tabuleiro[line - 1][column + 1];
+			let three = tabuleiro[line - 2][column + 2];
+			let four = tabuleiro[line - 3][column + 3];
+			if(one === 1 && one !== 0 && one === two && one === three && one === four){
+				return console.log("Player1 Win")
+			}
+			if(one === 2 && one !== 0 && one === two && one === three && one === four){
+				return console.log("Player2 Win")
+			}
+		}
+	}
+
+	return console.log("continue jogando")
 }
 
