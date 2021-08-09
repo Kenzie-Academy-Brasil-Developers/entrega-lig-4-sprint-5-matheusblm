1fc728165f4b272f9249667d4c9f4710a5c75881
Matheus Bueno <matheus_bueno10@hotmail.com>
2021-08-09 12:02:06 -0300

    Adicionado alerta de vitoria

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ let tabuleiro = [
 [0,0,0,0,0,0],
 [0,0,0,0,0,0],
 ];
+
+function alertWin(jogador){
+	let div = document.getElementById("container")
+	let alert = document.createElement("span")
+	alert.classList.add("alertWin")
+	alert.append("Parabens " + jogador + " voce ganhou!")
+	div.appendChild(alert)
+	setTimeout(function(){
+		alert.classList.add("hidden")
+		}, 5000)
+}
