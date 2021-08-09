13521255a3b10a1aaf7edcc21c08dfd6c571e025
NatanWS <natanwsn@gmail.com>
2021-08-09 16:04:30 -0300

    Criado check horizontal de vitória

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 let tabuleiro = [
-[0,0,0,0,0,0],
-[0,0,0,0,0,0],
-[0,0,0,0,0,0],
-[0,0,0,0,0,0],
-[0,0,0,0,0,0],
-[0,0,0,0,0,0],
-[0,0,0,0,0,0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
 ];
+
+function checkHorizontal(x, y) {
+    //check posições 1, 2, 3 e 4
+    let countPlayer1 = 0
+    let countPlayer2 = 0
+    let currentPosition = x
+    for (let position = currentPosition; position > currentPosition - 4; position--) {
+        countPlayer1 = 0
+        countPlayer2 = 0
+        for (let i = position; i < position + 4; i++) {
+            if (tabuleiro[i][y] === 1) {
+                countPlayer1++
+            }
+
+            if (tabuleiro[i][y] === 2) {
+                countPlayer2++
+            }
+        }
+
+        if (countPlayer1 === 4) {
+            console.log('Player One Wins!')
+        }
+
+        if (countPlayer2 === 4) {
+            console.log('Player Two Wins!')
+        }
+    }
+}
