db220775ad79eb88d4f51081f58b7b4c7952b62c
NatanWS <natanwsn@gmail.com>
2021-08-09 11:58:40 -0300

    Criada função para criar tabuleiro e style basico

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 let tabuleiro = [
-[0,0,0,0,0,0],
-[0,0,0,0,0,0],
-[0,0,0,0,0,0],
-[0,0,0,0,0,0],
-[0,0,0,0,0,0],
-[0,0,0,0,0,0],
-[0,0,0,0,0,0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
 ];
+
+function createBoard() {
+    let gameContainer = document.getElementById('container')
+
+    for (let i = 0; i < 7; i++) {
+        let boardColumn = document.createElement('div')
+        boardColumn.setAttribute('id', `column__${i+1}`)
+        boardColumn.classList.add('column')
+        gameContainer.appendChild(boardColumn)
+    }
+}
+
+createBoard() //inserir esta chamada em um botão de start
